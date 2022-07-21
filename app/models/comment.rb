f53713fd4cd2fd27ed17228d801bff506c74bda9
Pickles <cheesemaster56@gmail.com>
2022-07-21 03:54:02 -0400

    working comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
     validates :body, presence: true
     validates :user_id, presence: true
     validates :post_id, presence: true
